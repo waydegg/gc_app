945d30a104e3e69c3dc25756e9a22ade3e817368
waydegg <ztboneg@gmail.com>
2019-04-10 21:33:28 -0700

    testing stuff

--- app/server.py
+++ app/server.py
@@ -15,21 +15,20 @@ model_file_name = 'center_text__final_gen'
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
-# # GAN Stuff
-# PATH = Path('data')
-# CLEAN = Path(PATH/'clean')
-# MARKED = Path(PATH/'marked')
-# src = ImageImageList.from_folder(MARKED).split_by_rand_pct(0.1, seed=42)
-
-# def get_data(bs,size,src,clean_path):
-#     tfms = get_transforms(do_flip=False, max_rotate=5.0, max_zoom=1.05, max_warp=0)
+# GAN Stuff
+def get_data(bs,size,src,clean_path):
+    tfms = get_transforms(do_flip=False, max_rotate=5.0, max_zoom=1.05, max_warp=0)
     
-#     data = (src.label_from_func(lambda x: clean_path/x.name)
-#            .transform(tfms, size=size, tfm_y=True)
-#            .databunch(bs=bs).normalize(imagenet_stats, do_y=True))
+    data = (src.label_from_func(lambda x: clean_path/x.name)
+           .transform(tfms, size=size, tfm_y=True)
+           .databunch(bs=bs).normalize(imagenet_stats, do_y=True))
 
-#     data.c = 3
-#     return data
+    data.c = 3
+    return data
+
+def create_gen_learner():
+    return unet_learner(data_gen, arch, wd=wd, blur=True, norm_type=NormType.Weight,
+                         self_attention=True, y_range=y_range, loss_func=loss_gen)
 
 # App Stuff
 app = Starlette()
@@ -43,20 +42,36 @@ async def download_file(url, dest):
             data = await response.read()
             with open(dest, 'wb') as f: f.write(data)
 
-async def setup_learner():
-    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-    learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
-    learn.load(model_file_name)
-    return learn
+# async def setup_learner():
+#     await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
+#     data_bunch = ImageDataBunch.single_from_classes(path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+#     learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
+#     learn.load(model_file_name)
+#     return learn
 
 async def setup_gan_learner():
     await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
 
+    arch = models.resnet34
+    src = ImageImageList.from_folder(MARKED).random_split_by_pct(0.1, seed=42)
+    wd = 1e-3
+    y_range = (-3.,3.)
+    loss_gen = MSELossFlat()
 
+    data_gen = get_data(bs, size, src, CLEAN)
+
+    learn_gen = create_gen_learner()
+    os.system(f"mv {MARKED}/'models' {PATH}")
+    learn_gen.path = PATH
+
+    learn_gen.load(model_file_name)
+    learn_gen = learn
+
+    return learn
 
 loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future(setup_learner())]
+# tasks = [asyncio.ensure_future(setup_learner())]
+tasks = [asyncio.ensure_future(setup_gan_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
