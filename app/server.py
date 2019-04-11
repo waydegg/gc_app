9dadadf8be6caa67d6429bde4014e960d5d66451
waydegg <ztboneg@gmail.com>
2019-04-10 21:06:45 -0700

    testing model size

--- app/server.py
+++ app/server.py
@@ -10,12 +10,28 @@ from fastai.vision import *
 
 # model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
 # model_file_name = 'model'
-model_file_url = 'https://drive.google.com/uc?export=download&id=1tghKa7V_DEC9-X1eBJEbBN_zJZzKaatu'
-model_file_name = 'export'
+model_file_url = 'https://drive.google.com/uc?export=download&id=1mqJytX0sLwOJhkGtDCdb8MVqW1qNpvjk'
+model_file_name = 'center_text__final_gen'
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
+# # GAN Stuff
+# PATH = Path('data')
+# CLEAN = Path(PATH/'clean')
+# MARKED = Path(PATH/'marked')
+# src = ImageImageList.from_folder(MARKED).split_by_rand_pct(0.1, seed=42)
 
+# def get_data(bs,size,src,clean_path):
+#     tfms = get_transforms(do_flip=False, max_rotate=5.0, max_zoom=1.05, max_warp=0)
+    
+#     data = (src.label_from_func(lambda x: clean_path/x.name)
+#            .transform(tfms, size=size, tfm_y=True)
+#            .databunch(bs=bs).normalize(imagenet_stats, do_y=True))
+
+#     data.c = 3
+#     return data
+
+# App Stuff
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
@@ -33,6 +49,11 @@ async def setup_learner():
     learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
     learn.load(model_file_name)
     return learn
+
+async def setup_gan_learner():
+    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
+
+
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
